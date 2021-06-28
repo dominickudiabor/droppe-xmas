458c570e89327923cc3b1b6b02ac1a9514b35266
dominic kudiabor <dominickudd@gmail.com>
2021-06-28 19:50:30 +0300

    initial commit

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -8,7 +8,7 @@ export default function Product() {
   const { id } = useParams()
 
   const product = useSelector((state: AppState) =>
-    state.product.inCart.find(p => p.id === id)
+    state.product.inCart.find((p) => p.id === id)
   )
 
   if (!product) {
