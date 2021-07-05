77960b3157cf042cadab505233248bbe1decd1c7
dominic kudiabor <dominickudd@gmail.com>
2021-07-05 23:17:01 +0300

    added open sans google fonts

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { PersistGate } from 'redux-persist/integration/react'
 import App from './App'
+import './index.css'
 import makeStore from './redux/store'
 import * as serviceWorker from './serviceWorker'
 
